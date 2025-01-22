7bb552f8cceee61ab21c59fb55c9a235ae3b419f
Srinu Kallepalli <srinukallepalli16@gmail.com>
2025-01-22 14:53:57 +0000

    updates

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,10 +1,14 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageBox"
+], (Controller,MessageBox) => {
     "use strict";
 
     return Controller.extend("zhcp.controller.View1", {
         onInit() {
+        },
+        messagebox:function(){
+           MessageBox.success("Success Message");
         }
     });
 });
